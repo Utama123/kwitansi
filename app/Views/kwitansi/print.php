81e1d6ccee74c807d325850fc2653242b75591fe
Utama123 <pramancautama@gmail.com>
2025-01-18 12:40:41 +0700

    menambahkan fungsi terbilang secara langsung pada view

--- app/Views/kwitansi/print.php
+++ app/Views/kwitansi/print.php
@@ -9,6 +9,33 @@
     </style>
 </head>
 <body>
+<?php
+    function terbilang($angka)
+    {
+        $angka = abs($angka);
+        $angkaText = [
+            '', 'satu', 'dua', 'tiga', 'empat', 'lima',
+            'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'
+        ];
+        $result = '';
+
+        if ($angka < 12) {
+            $result = ' ' . $angkaText[$angka];
+        } elseif ($angka < 20) {
+            $result = terbilang($angka - 10) . ' belas';
+        } elseif ($angka < 100) {
+            $result = terbilang(floor($angka / 10)) . ' puluh' . terbilang($angka % 10);
+        } elseif ($angka < 1000) {
+            $result = terbilang(floor($angka / 100)) . ' ratus' . terbilang($angka % 100);
+        } elseif ($angka < 1000000) {
+            $result = terbilang(floor($angka / 1000)) . ' ribu' . terbilang($angka % 1000);
+        } elseif ($angka < 1000000000) {
+            $result = terbilang(floor($angka / 1000000)) . ' juta' . terbilang($angka % 1000000);
+        }
+
+        return trim($result);
+    }
+    ?>
     <h1>Kwitansi</h1>
     <p>No Kwitansi: <?= $kwitansi['no_kwitansi']; ?></p>
     <p>Nama Penerima: <?= $kwitansi['nama_penerima']; ?></p>
